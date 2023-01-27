f090470fe55c5c918f91f56d09e7a966f9d69f2a
Necati Turan <nhturan@gmail.com>
2023-01-27 17:36:54 +0300

    re,os, removeEND

--- removeEnd.py
+++ removeEnd.py
@@ -1,8 +1,10 @@
 import glob
 import os
+import shutil
+import re
 
 folder_path = 'C:\\Users\\nhtur\\Downloads\\endsubs'
-
+destination_folder = 'C:\\Users\\nhtur\\Downloads\\endsubs\\output'
 # for file_path in glob.glob(folder_path + '/*.srt'):
 #     with open(file_path, 'r') as file:
 #         file_name = os.path.basename(file_path)
@@ -27,3 +29,21 @@ folder_path = 'C:\\Users\\nhtur\\Downloads\\endsubs'
 #     new_file_name = file_name[:start_index] + '.srt'
 #     new_file_path = os.path.join(folder_path, new_file_name)
 #     os.rename(file_path, new_file_path)
+
+# for file_path in glob.glob(folder_path + '/*.srt'):
+#     file_name = os.path.basename(file_path)
+#     if '(' in file_name:
+#         start_index = file_name.index(' (')
+#         new_file_name = file_name[:start_index] + '.srt'
+#         if new_file_name != file_name:
+#             new_file_path = os.path.join(destination_folder, new_file_name)
+#             os.rename(file_path, new_file_path)
+#             shutil.copy2(new_file_path, destination_folder)
+#     else:
+#         shutil.copy2(file_path, destination_folder)
+
+for filename in os.listdir(folder_path):
+    if filename.endswith('.srt'):
+        new_filename = re.sub(r'\s+\(Transcribed.*\)', '', filename)
+        os.rename(os.path.join(folder_path, filename),
+                  os.path.join(folder_path, new_filename))
